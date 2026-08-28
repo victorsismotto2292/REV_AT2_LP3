5079499361065b48c946632107965de4984a3d32
= <167320276+victorsismotto2292@users.noreply.github.com>
2026-08-27 22:50:53 -0300

    Teste 3 - Ion Range
    Pasta 3 com o conteúdo de ion-range

---
+++ teste3-ionrange/src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular/lazy';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  constructor() {}
+  valorRange = 0;
+
+  onIonChange(ev: RangeCustomEvent){
+    this.valorRange = parseInt(ev.detail.value.toString());
+  }
+
+}
